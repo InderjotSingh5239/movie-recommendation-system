cf3229628d314f483adc265c601c00f9ce44d13a
InderjotSingh5239 <inderjotsingharora31@gmail.com>
2026-03-08 12:42:57 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,33 +1,17 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
+# load dataset
 movies = pd.read_csv("movies.csv")
 
+# fill empty values
 movies = movies.fillna('')
 
-movies["tags"] = movies["genre"] + " " + movies["overview"]
+def recommend(selected_genre):
 
-cv = CountVectorizer(max_features=5000, stop_words='english')
+    # filter movies by genre
+    filtered_movies = movies[movies["genre"] == selected_genre]
 
-vectors = cv.fit_transform(movies["tags"]).toarray()
-
-similarity = cosine_similarity(vectors)
-
-def recommend(movie):
-
-    movie_index = movies[movies['title'] == movie].index[0]
-
-    distances = similarity[movie_index]
-
-    movie_list = sorted(list(enumerate(distances)),
-                        reverse=True,
-                        key=lambda x: x[1])[1:6]
-
-    recommended_movies = []
-
-    for i in movie_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
-
+    # take first 5 movies
+    recommended_movies = filtered_movies["title"].head(5).tolist()
 
     return recommended_movies
